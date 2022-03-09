6b67556462bb03b4fa83ef436a546db99c2d01a3
menthus123 <MarvelJohnson@Hotmail.com>
2022-03-09 11:31:22 -0800

    Setup CORS to allow all origins

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
@@ -7,4 +7,6 @@ def file_upload():
   if request.method == 'POST' and request.content_length <= 1_000_000:
     uploaded_file = request.get_data(cache=False, as_text=True)
     print(uploaded_file)
-    return "<p>Received file!</p>"
+    res = Response(response="<p>Received file!</p>")
+    res.access_control_allow_origin = '*'
+    return res
