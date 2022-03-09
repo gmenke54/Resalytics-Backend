820695a0ea1e2a9a22c97e681b0799356aa8df7a
menthus123 <MarvelJohnson@Hotmail.com>
2022-03-09 11:38:56 -0800

    Printing files from request

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route("/", methods=['POST'])
 def file_upload():
   if request.method == 'POST' and request.content_length <= 1_000_000:
-    uploaded_file = request.get_data(cache=False, as_text=True)
-    print(uploaded_file)
+    files = request.files
+    print(files)
     res = Response(response="<p>Received file!</p>")
     res.access_control_allow_origin = '*'
     return res
