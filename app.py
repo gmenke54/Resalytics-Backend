a6554d415bd294c68a55924ed8b3bdc6e89b479f
menthus123 <MarvelJohnson@Hotmail.com>
2022-03-09 11:14:02 -0800

    Setup check for what is received when uploading file

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
-def hello_world():
-  if request.method == 'POST':
-    return "<p>Hello, World!</p>"
+def file_upload():
+  if request.method == 'POST' and request.content_length <= 1_000_000:
+    uploaded_file = request.get_data(cache=False, as_text=True)
+    print(uploaded_file)
+    return "<p>Received file!</p>"
