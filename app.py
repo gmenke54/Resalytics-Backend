df4e1102c1bcf5ae9afa80fe32bd1d3b76ecf16a
menthus123 <MarvelJohnson@Hotmail.com>
2022-03-09 12:06:25 -0800

    Reading uploaded file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def file_upload():
   if request.method == 'POST' and request.content_length <= 1_000_000:
     files = request.files
-    print(files['file'])
+    print(files['file'].read())
     res = Response(response="<p>Received file!</p>")
     res.access_control_allow_origin = '*'
     return res
